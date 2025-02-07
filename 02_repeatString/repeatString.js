18b9c17dce6efceee810a26bfd5925fa0f38224c
TheSoulCat <thesoulcat@outlook.com>
2025-02-06 22:31:08 -0500

    Tests failed.  Saving code to work on it later

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let arr = [];
+    for (let i=0; i < num; i++) {
+        arr.push(string);
+    };
+    const result = arr.join("");
+    return result;
 };
 
 // Do not edit below this line
