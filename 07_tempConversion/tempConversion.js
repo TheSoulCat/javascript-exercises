61cc52996d0d913c19a664159090deaaf88558ed
TheSoulCat <thesoulcat@outlook.com>
2025-02-25 22:50:34 -0500

    Completed Exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fah) {
+  const product = (fah - 32) * 5;
+  const result = product / 9;
+  return Math.round(result * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel) {
+  const product = cel * 9;
+  const quotient = product / 5;
+  const result = quotient + 32;
+  return Math.round(result * 10) / 10;
 };
 
 // Do not edit below this line
